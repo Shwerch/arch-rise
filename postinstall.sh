6587b78d286981cbf07e666af055d8815b9a2317
Shwerch <ilia.popov06@gmail.com>
2025-03-16 11:51:57 +0700

    Updated applications list

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,6 @@
 SCRIPT_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_DIR=$(basename $SCRIPT_PATH)
 
-FIRMWARE="ast-firmware wd719x-firmware"
-
 sudo ufw allow 53317/tcp
 sudo ufw allow 53317/udp
 sudo ufw reload
@@ -20,27 +18,34 @@ mkdir ~/.config/fastfetch
 cp $SCRIPT_PATH/configs/config.jsonc ~/.config/fastfetch
 echo "fastfetch" >> ~/.bashrc
 
-BROWSER="io.github.ungoogled_software.ungoogled_chromium io.gitlab.librewolf-community"
-OFFICE="org.onlyoffice.desktopeditors"
-CODE_EDITOR="com.vscodium.codium"
-MESSENGER="org.telegram.desktop dev.vencord.Vesktop"
-TORRENT="org.qbittorrent.qBittorrent"
-VIDEOEDITOR="org.kde.kdenlive"
-FILE_SHARER="org.localsend.localsend_app"
-YOUTUBE="io.freetubeapp.FreeTube com.github.unrud.VideoDownloader"
-TRANSLATOR="app.drey.Dialect"
-CALCULATOR="org.gnome.Calculator"
-IMAGE_VIEWER="org.gnome.Loupe"
-
-flatpak install flathub $BROWSER $OFFICE $CODE_EDITOR $MESSENGER $TORRENT $VIDEOEDITOR $FILE_SHARER $YOUTUBE $TRANSLATOR $CALCULATOR $IMAGE_VIEWER
-
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 rm -rf paru
 
-paru -S portproton cachyos-ananicy-rules-git $FIRMWARE \
-ttf-symbola ttf-impallari-cantora ttf-courier-prime ttf-gelasio-ib ttf-merriweather ttf-signika consolas-font apple-fonts
+BROWSER="io.github.ungoogled_software.ungoogled_chromium io.gitlab.librewolf-community"
+OFFICE="org.onlyoffice.desktopeditors org.libreoffice.LibreOffice"
+CODE_EDITOR="com.vscodium.codium"
+MESSENGER="org.telegram.desktop dev.vencord.Vesktop"
+TORRENT="org.qbittorrent.qBittorrent"
+FILE_SHARER="org.localsend.localsend_app"
+YOUTUBE="io.freetubeapp.FreeTube com.github.unrud.VideoDownloader"
+TRANSLATOR="app.drey.Dialect"
+CALCULATOR="org.gnome.Calculator"
+VIDEO="org.kde.haruna org.kde.kdenlive"
+IMAGE="org.kde.gwenview org.gimp.GIMP"
+AUDIO="org.tenacityaudio.Tenacity"
+ISO_WRITER="org.kde.isoimagewriter"
+
+GAMES="portproton"
+VENTOY="ventoy-bin"
+PERFORMANCE="cachyos-ananicy-rules-git"
+FIRMWARE="ast-firmware wd719x-firmware"
+FONTS="ttf-symbola ttf-impallari-cantora ttf-courier-prime ttf-gelasio-ib ttf-merriweather ttf-signika consolas-font apple-fonts"
 # ttf-source-sans-pro-ibx
+
+flatpak install flathub $BROWSER $OFFICE $CODE_EDITOR $MESSENGER $TORRENT $FILE_SHARER $YOUTUBE $TRANSLATOR $CALCULATOR $VIDEO $IMAGE $AUDIO $ISO_WRITER
+
+paru -S $GAMES $VENTOY $PERFORMANCE $FIRMWARE $FONTS
 sudo systemctl restart ananicy-cpp
